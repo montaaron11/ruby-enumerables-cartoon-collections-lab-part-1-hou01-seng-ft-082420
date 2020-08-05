895daffa78e58d9a7c41ac9eb81f273c4ff8c2c8
Aaron Montgomery <montaaron11@gmail.com>
2020-08-05 08:20:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,5 +13,3 @@ end
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
-animals = ["cat", "dog", "tiger"]
-animals.each_with_index { |animal, idx| puts "We have a #{animal} with index #{idx}" }
